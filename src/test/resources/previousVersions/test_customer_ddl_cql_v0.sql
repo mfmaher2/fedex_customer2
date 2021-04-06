c114dedff63542271431a431832f6796b5f2d5da
Mike Downie <michael.downie@experoinc.com>
2021-04-06 17:01:12 -0500

    WIP- CICS model, remove some duplication, combine tables with opco field where possible

--- src/test/resources/previousVersions/test_customer_ddl_cql_v0.sql
+++ src/test/resources/previousVersions/test_customer_ddl_cql_v0.sql
@@ -163,7 +163,7 @@ CREATE TABLE IF NOT EXISTS cics (
     --express_account_receivables
     account_receivables__call_zone_desc text,
     account_receivables__gsp_write_off text,
-    account_receivables__online_eligibility boolean,
+    account_receivables__onlineEligibility boolean,
     account_receivables__partial_pay_letter_flag boolean,
     account_receivables__payment_type text,
     account_receivables__payment_method_code text,
@@ -378,6 +378,13 @@ CREATE TABLE IF NOT EXISTS cics (
 
 
 
+
+
+
+
+
+
+
     PRIMARY KEY(account_number))
 WITH bloom_filter_fp_chance = 0.01
     AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
@@ -414,11 +421,176 @@ WITH bloom_filter_fp_chance = 0.01
     AND read_repair_chance = 0.0
     AND speculative_retry = '99PERCENTILE';
 
+CREATE TABLE IF NOT EXISTS cargo_account_receivables (
+    account_number text,
+    account_receivables__payor_type text,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
+
+CREATE TABLE IF NOT EXISTS express_account_receivables (
+    account_number text,
+    account_receivables__call_zone_desc text,
+    account_receivables__gsp_write_off text,
+    account_receivables__onlineEligibility boolean,
+    account_receivables__partial_pay_letter_flag boolean,
+    account_receivables__payment_type text,
+    account_receivables__payment_method_code text,
+    account_receivables__arrow_customer_flag text,
+    account_receivables__international_ar_preference int,   --does not follow standard capitalization patterns
+    account_receivables__international_ar_date date,        --does not follow standard capitalization patterns
+    account_receivables__no_refund_flag boolean,
+    account_receivables__debut_company_code text,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
+
+CREATE TABLE IF NOT EXISTS express_aggregations (
+    account_number text,
+    aggregations__ed_aggr_code text,
+    aggregations__geo_acct_number text,
+    aggregations__global_account_number text,
+    aggregations__global_subgroup text,
+    aggregations__ss_account_number text,
+    aggregations__bill_to_number text,
+    aggregations__edi_number text,
+    aggregations__copy_master_address boolean,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
+
 CREATE TABLE IF NOT EXISTS express_apply_discount_detail (
     account_number text,
     apply_discount__discount_flag boolean,
     apply_discount__effective_date_time timestamp,
     apply_discount__expiration_date_time timestamp,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
+
+
+CREATE TABLE IF NOT EXISTS express_credit_card (
+    account_number text,
+    credit_card set<frozen<express_credit_card_type>>,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
+
+CREATE TABLE IF NOT EXISTS express_credit_detail (
+    account_number text,
+    credit_detail__credit_status text,
+    credit_detail__credit_status_reason_code text,
+    credit_detail__denied_flag boolean,
+    credit_detail__bankruptcy_date date,
+    credit_detail__cash_only_date date,
+    credit_detail__cash_only_reason text,
+    credit_detail__credit_alert_detail text,
+    credit_detail__credit_alert_account_number text,
+    credit_detail__credit_alert_parent_type text,
+    credit_detail__credit_limit text,
+    credit_detail__credit_limit_tolerance_pct int,
+    credit_detail__override_date date,
+    credit_detail__credit_rating text,
+    credit_detail__receivership_account_number text,
+    credit_detail__receivership_date date,
+    credit_detail__rev_auth_id text,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
+
+CREATE TABLE IF NOT EXISTS express_edi (
+    account_number text,
+    edi__cust_inv_rept_flag boolean,
+    edi__dom_data_frmt text,
+    edi__dom_inv_print_until_date date,
+    edi__intl_data_frmt text,
+    edi__intl_inv_print_until_date date,
+    edi__mm_bill_3rd_party text,
+    edi__mm_bill_recip text,
+    edi__mm_bill_ship text,
+    edi__mm_bill_pwr_ship text,
+    edi__past_due_medium text,
+    edi__past_due_send_to text,
+    remit_frmt_vers text,
+    sep_exp_grnd_file boolean,
     PRIMARY KEY(account_number))
 WITH bloom_filter_fp_chance = 0.01
     AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
@@ -467,6 +639,26 @@ WITH bloom_filter_fp_chance = 0.01
     AND min_index_interval = 128
     AND read_repair_chance = 0.0
     AND speculative_retry = '99PERCENTILE';
+
+
+CREATE TABLE IF NOT EXISTS express_gfbo_electronic_pay (
+    account_number text,
+    PRIMARY KEY(account_number))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
 
 CREATE TABLE IF NOT EXISTS account_contact (
     account_type text,
@@ -589,6 +781,34 @@ WITH bloom_filter_fp_chance = 0.01
     AND read_repair_chance = 0.0
     AND speculative_retry = '99PERCENTILE';
 
+CREATE TABLE IF NOT EXISTS express_account_creation_profile_combined (
+    account_number text,
+    opco text,
+    profile__customer_request_name text,
+    profile__creation_date date,
+    profile__employee_requester__opco text,
+    profile__employee_requester__number text,
+    profile__source_group text,
+    profile__source_dept text,
+    profile__source_system text,
+    profile__employee_creator_opco text,
+    profile__employee_creator_number text,
+    PRIMARY KEY(account_number, opco))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
+
 CREATE TABLE IF NOT EXISTS express_account_creation_profile (
     account_number text,
     profile__customer_request_name text,
@@ -644,35 +864,6 @@ WITH bloom_filter_fp_chance = 0.01
     AND read_repair_chance = 0.0
     AND speculative_retry = '99PERCENTILE';
 
-CREATE TABLE IF NOT EXISTS account_creation_profile_combined (
-    account_number text,
-    opco text,
-    profile__customer_request_name text,
-    profile__creation_date date,
-    profile__employee_requester__opco text,
-    profile__employee_requester__number text,
-    profile__source_group text,
-    profile__source_dept text,
-    profile__source_system text,
-    profile__employee_creator_opco text,
-    profile__employee_creator_number text,
-    PRIMARY KEY(account_number))
-WITH bloom_filter_fp_chance = 0.01
-    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
-    AND comment = ''
-    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
-    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
-    AND crc_check_chance = 1.0
-    AND dclocal_read_repair_chance = 0.0
-    AND default_time_to_live = 0
-    AND gc_grace_seconds = 864000
-    AND max_index_interval = 2048
-    AND memtable_flush_period_in_ms = 0
-    AND min_index_interval = 128
-    AND read_repair_chance = 0.0
-    AND speculative_retry = '99PERCENTILE';
-
-
 
 CREATE TABLE IF NOT EXISTS freight_account_creation_profile (
     account_number text,
@@ -712,39 +903,6 @@ CREATE TABLE IF NOT EXISTS freight_account_profile (
     profile__archive_reason_code text,
     profile__sales_rep__opco text,
     profile__sales_rep__number text,
-    profile__service_level text,
-    profile__scac_code text,
-    PRIMARY KEY(account_number))
-WITH bloom_filter_fp_chance = 0.01
-    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
-    AND comment = ''
-    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
-    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
-    AND crc_check_chance = 1.0
-    AND dclocal_read_repair_chance = 0.0
-    AND default_time_to_live = 0
-    AND gc_grace_seconds = 864000
-    AND max_index_interval = 2048
-    AND memtable_flush_period_in_ms = 0
-    AND min_index_interval = 128
-    AND read_repair_chance = 0.0
-    AND speculative_retry = '99PERCENTILE';
-
-CREATE TABLE IF NOT EXISTS account_profile_combined (
-    account_number text,
-    opco text,
-    profile__account_type text,
-    profile_account_sub_type text,
-    profile__customer_account_status text,
-    profile__duplicate_account_flag boolean,
-    profile__fdx_ok_to_call_flag boolean,
-    profile__archive_date date,
-    profile__archive_reason_code text,
-    profile__archive_options text,
-    profile__sales_rep__opco text,
-    profile__sales_rep__number text,
-    profile__cargo_ind text,
-    profile__pref_cust_flag boolean,
     profile__service_level text,
     profile__scac_code text,
     PRIMARY KEY(account_number))
@@ -807,4 +965,25 @@ WITH bloom_filter_fp_chance = 0.01
     AND read_repair_chance = 0.0
     AND speculative_retry = '99PERCENTILE';
 
+
+--CREATE TABLE IF NOT EXISTS enterprise_restrictions ( --?? Is this a 1:many relationship with enterprise profile?
+--    account_number text,
+--    restrict_type text,         --service or view
+--    restrict_opco,
+--    PRIMARY KEY(account_number, restrict_type, restrict_opco))
+--WITH CLUSTERING ORDER BY(restrict_type ASC, restrict_opco ASC)
+--    AND bloom_filter_fp_chance = 0.01
+--    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+--    AND comment = ''
+--    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+--    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+--    AND crc_check_chance = 1.0
+--    AND dclocal_read_repair_chance = 0.0
+--    AND default_time_to_live = 0
+--    AND gc_grace_seconds = 864000
+--    AND max_index_interval = 2048
+--    AND memtable_flush_period_in_ms = 0
+--    AND min_index_interval = 128
+--    AND read_repair_chance = 0.0
+--    AND speculative_retry = '99PERCENTILE';
 
