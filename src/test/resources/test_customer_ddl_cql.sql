4007d606c0582a946d11fee1d0265cf0cc49456b
Mike Downie <michael.downie@experoinc.com>
2021-04-28 09:08:50 -0500

    Temporarily restore test functionality with temp table -> may adjust DDL and data in future to remove

--- src/test/resources/test_customer_ddl_cql.sql
+++ src/test/resources/test_customer_ddl_cql.sql
@@ -811,3 +811,71 @@ CREATE TABLE IF NOT EXISTS group_info_v1 (
      AND min_index_interval = 128
      AND read_repair_chance = 0.0
      AND speculative_retry = '99PERCENTILE';
+
+
+--Temporary table for sample code and sample data functionality
+CREATE TABLE IF NOT EXISTS contact (
+    contact_document_id bigint, --added field for key
+    person__first_name text,
+    person__last_name text,
+    person__middle_name text,
+    person__prefix text,
+    person__suffix text,
+    person__title text,
+    person__gender text,
+    company_name text,
+    job_department text,
+    address__street_line text,
+    address__additional_line1 text,
+    address__additional_line2 text,
+    address__secondary__unit1 text,
+    address__secondary__value1 text,
+    address__secondary__unit2 text,
+    address__secondary__value2 text,
+    address__secondary__unit3 text,
+    address__secondary__value3 text,
+    address__secondary__unit4 text,
+    address__secondary__value4 text,
+    address__geo_political_subdivision1 text,
+    address__geo_political_subdivision2 text,
+    address__geo_political_subdivision3 text,
+    address__postal_code text,
+    address__country_code text,
+    address__override__reason_code text,
+    address__override__problem_flag text,
+    address__usps_carrier_route_id text,  -- does not follow standard capitalization pattern
+    address__usps_delivery_point_code text, -- does not follow standard capitalization pattern
+    address__usps_check_digit text, -- does not follow standard capitalization pattern
+    tele_com set<frozen<telecom_details_type>>,
+    pager_use text,
+    email text,
+    html_use text,
+    email_marketing_flag text,
+    language text,
+    written_marketing_method_type text,
+    store_id text,
+    division text,
+    attention_to text,
+    contact_preference_flag text,
+    additional_email_info__email text,  --these could also be a set of UDTs
+    additional_email_info__html_use text,
+    additional_email_info__email_marketing_flag text,
+    additional_email_info_2_email text,
+    additional_email_info2__html_use text,
+    additional_email_info2__email_marketing_flag text,
+    social_media set<frozen<social_media_type>>,
+    PRIMARY KEY(contact_document_id))
+WITH bloom_filter_fp_chance = 0.01
+    AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
+    AND comment = ''
+    AND compaction = {'class': 'org.apache.cassandra.db.compaction.LeveledCompactionStrategy', 'enabled': 'true', 'sstable_size_in_mb': '160', 'tombstone_compaction_interval': '86400', 'tombstone_threshold': '0.2', 'unchecked_tombstone_compaction': 'false'}
+    AND compression = {'chunk_length_in_kb': '64', 'class': 'org.apache.cassandra.io.compress.LZ4Compressor'}
+    AND crc_check_chance = 1.0
+    AND dclocal_read_repair_chance = 0.0
+    AND default_time_to_live = 0
+    AND gc_grace_seconds = 864000
+    AND max_index_interval = 2048
+    AND memtable_flush_period_in_ms = 0
+    AND min_index_interval = 128
+    AND read_repair_chance = 0.0
+    AND speculative_retry = '99PERCENTILE';
--- src/test/resources/test_customer_search_ddl_cql.sql
+++ src/test/resources/test_customer_search_ddl_cql.sql
@@ -1,8 +1,9 @@
 USE customer;
 CREATE SEARCH INDEX IF NOT EXISTS ON cust_acct_v1;
-CREATE SEARCH INDEX IF NOT EXISTS ON contact;
-CREATE SEARCH INDEX IF NOT EXISTS ON cust_acct_v1;
+CREATE SEARCH INDEX IF NOT EXISTS ON account_contact;
 CREATE SEARCH INDEX IF NOT EXISTS ON payment_info_v1;
 CREATE SEARCH INDEX IF NOT EXISTS ON assoc_accounts_v1;
 CREATE SEARCH INDEX IF NOT EXISTS ON national_account_v1;
 
+--Temporary search index for test functionality
+CREATE SEARCH INDEX IF NOT EXISTS ON contact;
