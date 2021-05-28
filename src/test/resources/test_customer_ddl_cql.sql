c993bce8e981572a675be9d2a11a13f6f2c09cae
Mike Downie <michael.downie@experoinc.com>
2021-05-28 15:40:18 -0500

    Modifications to line_of_business contact fields based on conversation with Harshul

--- src/test/resources/test_customer_ddl_cql.sql
+++ src/test/resources/test_customer_ddl_cql.sql
@@ -56,14 +56,6 @@ CREATE TYPE IF NOT EXISTS centralized_opco_description_type (
     opco_account_number text
 );
 
-CREATE TYPE IF NOT EXISTS line_of_business_contact_type(
-    contact_document_id text,
-    addl_copies int,
-    effective_date date,
-    expiration_date date,
-    comment text,
-);
-
 CREATE TYPE IF NOT EXISTS time_event_additional_details_items(
     name text,
     value text
@@ -921,10 +913,68 @@ CREATE TABLE IF NOT EXISTS line_of_business_v1 (
     line_of_business__preference__pri_bill_to_eff_date date,
     line_of_business__preference__pri_bill_to_exp_date date,
     line_of_business__preference__pri_bill_to_inv_copies int,
-    line_of_business__contact set<frozen<line_of_business_contact_type>>,
     total_addl_addresses int,
-    PRIMARY KEY(account_number, line_of_business__preference__invoice_type, opco))
- WITH CLUSTERING ORDER BY(line_of_business__preference__invoice_type ASC, opco ASC)
+
+    --line of business contacts
+    contact_document_id text,
+    addl_copies int,
+    effective_date date,
+    expiration_date date,
+    comment text,
+
+        --contact, same/similar to fields from account_contact
+    person__first_name text,
+    person__last_name text,
+    person__middle_name text,
+    person__prefix text,
+    person__suffix text,
+    person__title text,
+    person__gender text,
+    company_name text,
+    job_department text,
+    address__street_line text,
+    address__additional_line1 text,
+    address__additional_line2 text,
+    address__secondary__unit1 text,
+    address__secondary__value1 text,
+    address__secondary__unit2 text,
+    address__secondary__value2 text,
+    address__secondary__unit3 text,
+    address__secondary__value3 text,
+    address__secondary__unit4 text,
+    address__secondary__value4 text,
+    address__geo_political_subdivision1 text,
+    address__geo_political_subdivision2 text,
+    address__geo_political_subdivision3 text,
+    address__postal_code text,
+    address__country_code text,
+    address__override__reason_code text,
+    address__override__problem_flag text,
+    address__usps_carrier_route_id text,  -- does not follow standard capitalization pattern
+    address__usps_delivery_point_code text, -- does not follow standard capitalization pattern
+    address__usps_check_digit text, -- does not follow standard capitalization pattern
+    tele_com set<frozen<telecom_details_type>>,
+    pager_use text,
+    email text,
+    html_use text,
+    email_marketing_flag text,
+    language text,
+    written_marketing_method_type text,
+    store_id text,
+    division text,
+    attention_to text,
+    contact_preference_flag text,
+    additional_email_info__email text,  --these could also be a set of UDTs
+    additional_email_info__html_use text,
+    additional_email_info__email_marketing_flag text,
+    additional_email_info_2_email text,
+    additional_email_info2__html_use text,
+    additional_email_info2__email_marketing_flag text,
+    social_media set<frozen<social_media_type>>,
+
+
+    PRIMARY KEY(account_number, line_of_business__preference__invoice_type, opco, contact_document_id))
+ WITH CLUSTERING ORDER BY(line_of_business__preference__invoice_type ASC, opco ASC, contact_document_id ASC)
      AND bloom_filter_fp_chance = 0.01
      AND caching = {'keys': 'ALL', 'rows_per_partition': 'NONE'}
      AND comment = ''
