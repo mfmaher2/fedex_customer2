e2fe62b8aac514664a2533d60dd68167cb09f93a
Mike Downie <michael.downie@experoinc.com>
2022-05-30 18:24:36 -0500

    Intially generated scala module files

---
+++ cam_analytics/src/main/scala/datastax/com/cam_analytics/CAMAnalyticsApp.scala
@@ -0,0 +1,9 @@
+package datastax.com.cam_analytics
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ cam_analytics/src/test/scala/datastax/com/cam_analytics/AppTest.scala
@@ -0,0 +1,17 @@
+package datastax.com.cam_analytics
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
